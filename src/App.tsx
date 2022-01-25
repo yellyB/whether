c7a01b3e3442a44072af09aa9c6a9fa61ed6950f
bom\bomi <kk3p1q1@gmail.com>
2022-01-25 22:11:51 +0900

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import "./App.less";
+import "./style.css";
+
+const App = () => {
+  const [sideKey, setSideKey] = useState<string>("HOME");
+
+  // const handleSideKeyOnChange = (key: string) => {
+  //   setSideKey(key);
+  // };
+
+  useEffect(() => {}, []);
+
+  return <React.Fragment>fsdfsdf</React.Fragment>;
+};
+
+export default App;
---
+++ src/common/interface.tsx
@@ -0,0 +1,38 @@
+export interface IMovieList {
+  key: number;
+  title: string;
+  bookRate: string;
+  releaseDate: string;
+}
+
+export interface IMovieStartTimes {
+  name: string;
+  times: string[];
+}
+export interface IMovieTimesEachRoom {
+  room: string;
+  time: string;
+}
+export interface IComment {
+  writer: string;
+  movieKey: number;
+  content: string;
+  rate: number;
+  like: number;
+  dislike: number;
+  profileIdx: number;
+}
+
+export interface IBoard {
+  index: number;
+  title: string;
+  writer: string;
+  content: string;
+  viewCount: number;
+}
+
+export interface IAlarm {
+  index: number;
+  title: string;
+  content: string;
+}
