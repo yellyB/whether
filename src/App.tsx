2eb4bfbe1d9646a41b3c2ee3b3b6a3ee4873f1a0
yellyB <kk3p1q1@gmail.com>
2022-01-26 15:41:20 +0900

    데이터 조회

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
 import "./App.less";
 import "./style.css";
 
 const App = () => {
-  const [sideKey, setSideKey] = useState<string>("HOME");
+  const [params, setParams] = useState<[]>({
+    serviceKey: process.env.REACT_APP_API_KEY,
+    numOfRows: 50,
+    pageNo: 1,
+    dataType: "JSON",
+    base_date: "20220126",
+    base_time: "0500",
+    nx: 55,
+    ny: 127,
+  });
+  const url = `/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${params.serviceKey}&pageNo=1&numOfRows=${params.numOfRows}&dataType=${params.dataType}&base_date=${params.base_date}&base_time=${params.base_time}&nx=${params.nx}&ny=${params.ny}`;
 
-  // const handleSideKeyOnChange = (key: string) => {
-  //   setSideKey(key);
-  // };
-
-  useEffect(() => {}, []);
+  useEffect(() => {
+    Axios.get(url).then((response) => {
+      console.log(response);
+      console.log(response.data);
+    });
+  }, []);
 
   return <React.Fragment>fsdfsdf</React.Fragment>;
 };
