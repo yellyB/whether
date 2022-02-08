b6a0cb9b3376f1aab65a0f95e55a3c5c937d4bfb
bom\bomi <kk3p1q1@gmail.com>
2022-02-08 20:48:28 +0900

    현재 시각 표시 추가

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,9 +1,40 @@
 import React, { useState, useEffect } from "react";
 
 const Time = () => {
-  useEffect(() => {}, []);
+  const [date, setDate] = useState("00 : 00 : 00");
+  const [time, setTime] = useState("00 : 00 : 00");
 
-  return <div className="time_wrapper">현재 시각</div>;
+  const currentDate = () => {
+    const now = new Date();
+    const year = String(now.getFullYear());
+    const month = String(now.getMonth());
+    const day = String(now.getDate());
+    setDate(`${year}년 ${month}월 ${day}일`);
+  };
+
+  const currentTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    setTime(`${hours} : ${minutes} : ${seconds}`);
+  };
+
+  const startTimer = () => {
+    setInterval(currentTime, 1000);
+  };
+
+  useEffect(() => {
+    currentDate();
+    startTimer();
+  }, []);
+
+  return (
+    <div className="time_wrapper">
+      현재 시각{date}
+      {time}
+    </div>
+  );
 };
 
 export default Time;
