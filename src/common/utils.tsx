7a67214efc9d6f2c553e9f7f8a8fcfed2ac07fb0
yellyB <kk3p1q1@gmail.com>
2022-02-09 13:23:35 +0900

    추천 아이템 이미지 출력

--- src/common/utils.tsx
+++ src/common/utils.tsx
@@ -9,3 +9,9 @@ export const whetherState = (temporature: string, rain: string) => {
     return enSkyState.sunny;
   } else return enSkyState.cloudy;
 };
+
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+};
--- src/components/SiteList.tsx
+++ src/components/SiteList.tsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { ICody, IFcstData } from "../common/interface";
-import { getCody } from "../common/api";
+import { getRandomInt } from "../common/utils";
 
 const SiteList = (props: {}) => {
-  return <div className="siteList_wrapper">site</div>;
+  console.log(getRandomInt(10, 20));
+
+  return (
+    <div className="siteList_wrapper">
+      {Array.from({ length: 4 }, (v, i) => i).map((i: number) => (
+        <img
+          className="siteList_img"
+          alt="."
+          src={
+            process.env.PUBLIC_URL +
+            "/images/items/" +
+            getRandomInt(i * 10, (i + 1) * 10) +
+            ".png"
+          }
+        />
+      ))}
+    </div>
+  );
 };
 
 export default SiteList;
