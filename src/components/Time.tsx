717591b6b9def511fad24e1d1a269f32a7d8c262
yellyB <kk3p1q1@gmail.com>
2022-02-12 14:19:30 +0900

    요일 출력

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -4,12 +4,41 @@ const Time = () => {
   const [date, setDate] = useState("0000년 00월 00일");
   const [time, setTime] = useState("00:00:00");
 
+  const getDayOfWeek = (val) => {
+    let result = "";
+    switch (val) {
+      case 0:
+        result = "일";
+        break;
+      case 1:
+        result = "월";
+        break;
+      case 2:
+        result = "화";
+        break;
+      case 3:
+        result = "수";
+        break;
+      case 4:
+        result = "목";
+        break;
+      case 5:
+        result = "금";
+        break;
+      case 6:
+        result = "토";
+        break;
+    }
+    return result;
+  };
+
   const currentDate = () => {
     const now = new Date();
     const year = String(now.getFullYear());
     const month = String(now.getMonth());
-    const day = String(now.getDate());
-    setDate(`${year}년 ${month}월 ${day}일`);
+    const date = String(now.getDate());
+    const day = getDayOfWeek(now.getDay());
+    setDate(`${month}월 ${date}일 ${day}요일`);
   };
 
   const currentTime = () => {
