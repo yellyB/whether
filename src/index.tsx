a745975363837cba8442eb17372cd22aac9c0a23
bom\bomi <kk3p1q1@gmail.com>
2022-02-24 23:21:08 +0900

    axios base url 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://apis.data.go.kr";
 
 ReactDOM.render(
   <React.StrictMode>
